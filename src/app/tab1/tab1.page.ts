c5cf5cf0f2ee9429c1d648f8f35db8f3dd09babd
uday-engg-sdk <banavath.naik@moengage.com>
2023-12-05 09:39:53 +0530

    adding new changes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CallBackData, sample } from 'sampleplugin';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor() {
+    console.log('Uday');
+    sample.echo({value: 'Rajesh'}).then((response: { value: any; }) => {
+     // console.log("ervferf");
+      console.log(response.value);
+  });
+  sample.addListener("ApplicationGoingToBackground",(data: CallBackData) => {
+   console.log("Application did Enter Background" + data);
+  });
+ }
 }
